cf8707cb77b2fc821a8ddf2050deb5c74794b1f9
Manuel della Gala <manuel.dellagala@gmail.com>
2021-05-04 23:34:30 +0200

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="App">
     <Header/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    Header,
+    Header,Footer,
   }
 }
 </script>
