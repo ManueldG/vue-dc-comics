85f3740f239c21014f21ba97eea9ad4bda476a38
Manuel della Gala <manuel.dellagala@gmail.com>
2021-05-04 19:49:25 +0200

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+    <header id="Header">
+        <div class="logo">
+            <img src="../../../img/dc-logo.png" alt="LOGO">
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name : "Header",
+ 
+}
+</script>
+
+<style>
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+li a {
+    text-decoration: none;
+    color: black;
+}
+
+li a:hover {
+    border-bottom: 2px solid blue;
+}
+</style>
